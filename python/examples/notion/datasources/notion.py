0c90ad0f863203fddcd8c7105c23932c9e5663e4
Yeuoly <admin@srmxy.cn>
2025-05-26 17:44:12 +0800

    feat: impl datasource oauth

--- python/examples/notion/datasources/notion.py
+++ python/examples/notion/datasources/notion.py
@@ -11,6 +11,10 @@ from dify_plugin.interfaces.datasource.online_document import OnlineDocumentData
 
 
 class NotionDatasource(OnlineDocumentDatasource):
+    _NOTION_PAGE_SEARCH = "https://api.notion.com/v1/search"
+    _NOTION_BLOCK_SEARCH = "https://api.notion.com/v1/blocks"
+    _NOTION_BOT_USER = "https://api.notion.com/v1/users/me"
+
     def _get_pages(self, datasource_parameters: Mapping[str, Any]) -> GetOnlineDocumentPagesResponse:
         return GetOnlineDocumentPagesResponse(result=[])
 
--- python/examples/notion/provider/notion.py
+++ python/examples/notion/provider/notion.py
@@ -1,17 +1,57 @@
 from collections.abc import Mapping
 from typing import Any
+import urllib.parse
 
+import requests
 from werkzeug import Request
 
 from dify_plugin.interfaces.datasource import DatasourceProvider
 
 
 class NotionDatasourceProvider(DatasourceProvider):
+    _AUTH_URL = "https://api.notion.com/v1/oauth/authorize"
+    _TOKEN_URL = "https://api.notion.com/v1/oauth/token"
+
     def _oauth_get_authorization_url(self, system_credentials: Mapping[str, Any]) -> str:
-        return super()._oauth_get_authorization_url(system_credentials)
+        """
+        Generate the authorization URL for the Notion OAuth.
+        """
+        params = {
+            "client_id": system_credentials["client_id"],
+            "response_type": "code",
+            "redirect_uri": system_credentials["redirect_uri"],
+            "owner": "user",
+        }
+        return f"{self._AUTH_URL}?{urllib.parse.urlencode(params)}"
 
     def _oauth_get_credentials(self, system_credentials: Mapping[str, Any], request: Request) -> Mapping[str, Any]:
-        return super()._oauth_get_credentials(system_credentials, request)
+        """
+        Get the credentials for the Notion OAuth.
+        """
+        code = request.args.get("code")
+        if not code:
+            raise ValueError("No code provided")
+
+        data = {"code": code, "grant_type": "authorization_code", "redirect_uri": system_credentials["redirect_uri"]}
+        headers = {"Accept": "application/json"}
+        auth = (system_credentials["client_id"], system_credentials["client_secret"])
+        response = requests.post(self._TOKEN_URL, data=data, auth=auth, headers=headers)
+        response_json = response.json()
+        access_token = response_json.get("access_token")
+        if not access_token:
+            raise ValueError(f"Error in Notion OAuth: {response_json}")
+
+        workspace_name = response_json.get("workspace_name")
+        workspace_icon = response_json.get("workspace_icon")
+        workspace_id = response_json.get("workspace_id")
+        # get all authorized pages
+
+        return {
+            "access_token": access_token,
+            "workspace_name": workspace_name,
+            "workspace_icon": workspace_icon,
+            "workspace_id": workspace_id,
+        }
 
     def _validate_credentials(self, credentials: Mapping[str, Any]):
         """
