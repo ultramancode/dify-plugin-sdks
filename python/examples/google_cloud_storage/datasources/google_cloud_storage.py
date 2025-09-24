4821ef255246701f94f2234e0bda65402bfc00f9
Harry <xh001x@hotmail.com>
2025-09-24 14:34:21 +0800

    feat: add datasource plugin examples

--- python/examples/google_cloud_storage/datasources/google_cloud_storage.py
+++ python/examples/google_cloud_storage/datasources/google_cloud_storage.py
@@ -1,8 +1,7 @@
 import json
-from collections.abc import Generator, Mapping
-from typing import Any
-
-from google.cloud import storage
+import os
+from collections.abc import Generator
+from typing import Any, Mapping
 
 from dify_plugin.entities.datasource import (
     DatasourceMessage,
@@ -13,52 +12,56 @@ from dify_plugin.entities.datasource import (
     OnlineDriveFileBucket,
 )
 from dify_plugin.interfaces.datasource.online_drive import OnlineDriveDatasource
+from google.cloud import storage
 
 
 class GoogleCloudStorageDataSource(OnlineDriveDatasource):
-    def _browse_files(self, request: OnlineDriveBrowseFilesRequest) -> OnlineDriveBrowseFilesResponse:
+    def _browse_files(
+        self,  request: OnlineDriveBrowseFilesRequest
+    ) -> OnlineDriveBrowseFilesResponse:
         credentials = self.runtime.credentials.get("credentials")
         bucket_name = request.bucket
         prefix = request.prefix or ""
         max_keys = request.max_keys or 100
-        start_after = request.start_after or ""
+        next_page_parameters = request.next_page_parameters or {}
+
 
         if not credentials:
             raise ValueError("Credentials not found")
-
+        
+        
         client = storage.Client.from_service_account_info(json.loads(credentials))
         if not bucket_name:
             buckets = client.list_buckets()
-            file_buckets = [
-                OnlineDriveFileBucket(bucket=bucket.name, files=[], is_truncated=False) for bucket in buckets
-            ]
+            file_buckets = [OnlineDriveFileBucket(bucket=bucket.name, files=[], is_truncated=False, next_page_parameters={}) for bucket in buckets]
             return OnlineDriveBrowseFilesResponse(result=file_buckets)
         else:
-            if not start_after and prefix:
+            if not next_page_parameters and prefix:
                 max_keys = max_keys + 1
-            blobs = client.list_blobs(
-                bucket_name, prefix=prefix, max_results=max_keys, start_offset=start_after, delimiter="/"
-            )
+            blobs = client.list_blobs(bucket_name, 
+                                      prefix=prefix, 
+                                      max_results=max_keys, 
+                                      page_token=next_page_parameters.get("page_token"), 
+                                      delimiter="/")
             is_truncated = blobs.next_page_token is not None
+            next_page_parameters = {"page_token": blobs.next_page_token} if blobs.next_page_token else {}
             files = []
-            files.extend([OnlineDriveFile(key=blob.name, size=blob.size) for blob in blobs if blob.name != prefix])
+            files.extend([OnlineDriveFile(id=blob.name, name=os.path.basename(blob.name), size=blob.size, type="file") for blob in blobs if blob.name!=prefix])
             for prefix in blobs.prefixes:
-                if start_after and start_after == prefix:
+                if next_page_parameters and next_page_parameters == prefix:
                     continue
-                files.append(OnlineDriveFile(key=prefix, size=0))
-            file_bucket = OnlineDriveFileBucket(
-                bucket=bucket_name, files=sorted(files, key=lambda x: x.key), is_truncated=is_truncated
-            )
+                files.append(OnlineDriveFile(id=prefix, name=os.path.basename(prefix.rstrip('/')), size=0, type="folder"))
+            file_bucket = OnlineDriveFileBucket(bucket=bucket_name, files=sorted(files, key=lambda x: x.id), is_truncated=is_truncated, next_page_parameters=next_page_parameters)
             return OnlineDriveBrowseFilesResponse(result=[file_bucket])
 
     def _download_file(self, request: OnlineDriveDownloadFileRequest) -> Generator[DatasourceMessage, None, None]:
         credentials = self.runtime.credentials.get("credentials")
         bucket_name = request.bucket
-        key = request.key
+        key = request.id
 
         if not credentials:
             raise ValueError("Credentials not found")
-
+        
         if not bucket_name:
             raise ValueError("Bucket name not found")
 
@@ -72,23 +75,17 @@ class GoogleCloudStorageDataSource(OnlineDriveDatasource):
         service_account_obj = {
             key: credentials.get(key)
             for key in [
-                "project_id",
-                "private_key_id",
-                "private_key",
-                "client_email",
-                "client_id",
-                "client_x509_cert_url",
+                "project_id", "private_key_id", "private_key", 
+                "client_email", "client_id", "client_x509_cert_url"
             ]
         }
 
-        service_account_obj.update(
-            {
-                "type": "service_account",
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "universe_domain": "googleapis.com",
-            }
-        )
+        service_account_obj.update({
+            "type": "service_account",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "universe_domain": "googleapis.com",
+        })
 
         return service_account_obj
